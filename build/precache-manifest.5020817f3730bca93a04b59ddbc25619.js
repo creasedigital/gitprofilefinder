fb9a56cbf4a1df47eefd0780eaf02d1dbcb1dd48
Ojore Chris <ojorechris@gmail.com>
2020-05-06 00:32:11 +0100

    production build folder was added

---
+++ build/precache-manifest.5020817f3730bca93a04b59ddbc25619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e99305f1909c1d4c0a89c93b888abd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92212a7aa71ce38eac04",
+    "url": "/static/css/main.caefe0cf.chunk.css"
+  },
+  {
+    "revision": "38a35ed5ce7d648c7c27",
+    "url": "/static/js/2.f446edfd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f446edfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92212a7aa71ce38eac04",
+    "url": "/static/js/main.74c17ed3.chunk.js"
+  },
+  {
+    "revision": "e8ab3fb574cbb1d7071d",
+    "url": "/static/js/runtime-main.c6397bb9.js"
+  }
+]);
